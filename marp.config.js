16cc112ea6cd8195bca2cbfa9f2527e6d8a00d6e
Asuma Yamada <yamadaasuma@gmail.com>
2024-12-11 01:17:01 +0900

    feat: add marp configuration and update cursor rules for improved presentation formatting

---
+++ marp.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pdf: true,
+  allowLocalFiles: true,
+  options: {
+    pdf: {
+      printBackground: true,
+      format: 'A4',
+      preferCSSPageSize: true,
+      displayHeaderFooter: false,
+      margin: {
+        top: '0in',
+        right: '0in',
+        bottom: '0in',
+        left: '0in'
+      }
+    }
+  }
+}
